11b64966e1ecd4556198c754ee5c6f8e6a31e3e8
Affan Shahid <affan.shahid.94@gmail.com>
2024-07-29 10:19:40 +0500

    chore: bump version in generated header

--- lib/codegen.ts
+++ lib/codegen.ts
@@ -127,7 +127,7 @@ export async function generateGqlTagOperations(
 export function generateHeader(): string {
   return `// deno-lint-ignore-file
 
-import { TypedDocumentNode } from "https://deno.land/x/exograph_deno_utils@v0.0.3/mod.ts";
+import { TypedDocumentNode } from "https://deno.land/x/exograph_deno_utils@v0.0.4/mod.ts";
 
 `;
 }
